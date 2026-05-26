d38b2c76cf4a3567dbefc0bd2688347a3d00df67
Kirill Zemko <kirillzemko@yandex.ru>
2026-05-26 11:52:09 +0200

    initial commit

--- parser.js
+++ parser.js
@@ -1,8 +1,18 @@
 // @todo: напишите здесь код парсера
 
+const metaKeywords = document.querySelector('meta[name="keywords"]').content;
+const metaDescription = document.querySelector('meta[name="description"]').content;
+const ogTags = document.querySelectorAll('meta[property^="og:"]');
+
 function parsePage() {
+    const metaLang = document.querySelector('[lang]');
+    const metaTitle = document.title.split('—')[0].trim();
+
     return {
-        meta: {},
+        meta: {
+            title: metaTitle,
+            language: metaLang,
+        },
         product: {},
         suggested: [],
         reviews: []
