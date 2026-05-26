fca64f5b2fd5cece585dcf4b13ef3be29d5f9acd
Kirill Zemko <kirillzemko@yandex.ru>
2026-05-26 13:06:09 +0200

    Added getData

--- parser.js
+++ parser.js
@@ -1,17 +1,30 @@
 // @todo: напишите здесь код парсера
 
-const metaKeywords = document.querySelector('meta[name="keywords"]').content;
+const metaLang = document.querySelector('[lang]');
+const metaTitle = document.title.split('—')[0].trim();
+const metaKeywords = document.querySelector('meta[name="keywords"').content.split(', ');
 const metaDescription = document.querySelector('meta[name="description"]').content;
 const ogTags = document.querySelectorAll('meta[property^="og:"]');
 
+// функция преобразования в объект атрибутов property og
+function getOgData() {
+    const ogData = {};
+    ogTags.forEach(tag => {
+        const key = tag.getAttribute('property').replace('og:', '');
+        ogData[key] = tag.content;
+    });
+    return ogData;
+}
+
 function parsePage() {
-    const metaLang = document.querySelector('[lang]');
-    const metaTitle = document.title.split('—')[0].trim();
 
     return {
         meta: {
             title: metaTitle,
-            language: metaLang,
+            language: metaLang.lang,
+            description: metaDescription,
+            keywords: metaKeywords,
+            opengraph: getOgData(ogTags),
         },
         product: {},
         suggested: [],
